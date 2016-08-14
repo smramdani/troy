0c18638d6176d8d27183371ac4963497ab2210c4
OmniaGM <omnia.gamal1988@gmail.com>
2016-08-15 00:39:08 +0100

    #49 Implement InsertStatement parser

--- cql-ast/src/main/scala/troy/cql/ast/dml/InsertStatement.scala
+++ cql-ast/src/main/scala/troy/cql/ast/dml/InsertStatement.scala
@@ -3,21 +3,25 @@ package troy.cql.ast.dml
 import troy.cql.ast._
 
 case class InsertStatement(
-    tableName: TableName,
-    insertClause: InsertStatement.InsertClause,
-    ifNotExists: Boolean,
-    using: Seq[InsertStatement.UpdateParam]
+  into: TableName,
+  insertClause: InsertStatement.InsertClause,
+  ifNotExists: Boolean,
+  using: Option[Seq[InsertStatement.UpdateParam]]
 ) extends Cql3Statement
 object InsertStatement {
   sealed trait InsertClause
-  case class NamesValues(columnNames: Seq[Identifier], tupleLiteral: TupleLiteral) extends InsertClause
+  case class NamesValues(columnNames: Seq[Identifier], values: TupleLiteral) extends InsertClause
   case class JsonClause(value: String, default: Option[Default]) extends InsertClause
 
   sealed trait Default
-  case object Null extends Default
-  case object UNSET extends Default
+  case object NullValue extends Default
+  case object Unset extends Default
 
   sealed trait UpdateParam
-  case class UpdateValue(value: String) extends UpdateParam
-  case class UpdateVariable(bindMarker: BindMarker) extends UpdateParam
+  case class Timestamp(value: UpdateParamValue) extends UpdateParam
+  case class Ttl(value: UpdateParamValue) extends UpdateParam
+
+  sealed trait UpdateParamValue
+  case class UpdateValue(value: String) extends UpdateParamValue
+  case class UpdateVariable(bindMarker: BindMarker) extends UpdateParamValue
 }
---
+++ cql-parser/src/main/scala/troy/cql/parser/dml/InsertStatementParser.scala
@@ -0,0 +1,56 @@
+package troy.cql.parser.dml
+
+import troy.cql.ast.CqlParser._
+import troy.cql.ast.dml
+import troy.cql.ast.dml.InsertStatement
+
+trait InsertStatementParser {
+  def insertStatement: Parser[InsertStatement] = {
+    import InsertStatement._
+
+    def into = "INTO" ~> tableName
+
+    def insertClause: Parser[InsertClause] = {
+      def names = parenthesis(rep1sep(identifier, ","))
+
+      def namesValues: Parser[NamesValues] = names ~ ("VALUES".i ~> tupleLiteral) ^^^^ NamesValues
+
+      def jsonClause: Parser[JsonClause] = {
+        def default: Parser[Default] = {
+          def nullValue = "NULL".i ^^^ NullValue
+          def unset = "UNSET".i ^^^ Unset
+
+          nullValue | unset
+        }
+
+        "JSON" ~> Constants.string ~ ("DEFAULT".i ~> default).? ^^^^ JsonClause
+      }
+
+      namesValues | jsonClause
+    }
+
+    def ifNotExists = "IF NOT EXISTS".flag
+
+    def UpdateParamValue: Parser[UpdateParamValue] = {
+      def updateValue = Constants.integer ^^ UpdateValue
+      def updateVariable = bindMarker ^^ UpdateVariable
+
+      updateValue | updateVariable
+    }
+
+    def updateParam: Parser[UpdateParam] = {
+      def timestamp = "TIMESTAMP".i ~> UpdateParamValue ^^ Timestamp
+      def ttl = "TTL".i ~> UpdateParamValue ^^ Ttl
+
+      timestamp | ttl
+    }
+
+    def using = "USING".i ~> rep1sep(updateParam, "AND".i)
+
+    "INSERT".i ~>
+      into ~
+      insertClause ~
+      ifNotExists ~
+      using.? ^^^^ InsertStatement.apply
+  }
+}
